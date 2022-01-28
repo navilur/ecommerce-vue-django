35a3e1fc6521423d0356a0dc5c47f3c66eae4cee
navilur <navilur.rahman@gmail.com>
2022-01-29 01:52:21 +0600

    Create django app and models for products

--- ecommerce_django/ecommerce_django/settings.py
+++ ecommerce_django/ecommerce_django/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'corsheaders',
     'djoser',
+    'product',
 ]
 
 CORS_ALLOWED_ORIGINS =[
@@ -125,6 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOTS = BASE_DIR / 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
