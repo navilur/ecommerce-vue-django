c924f5709a21293bb47ea7b49a83e915140e4e14
navilur <navilur.rahman@gmail.com>
2022-01-29 13:43:15 +0600

    Create serializer and views for the products

--- ecommerce_django/product/models.py
+++ ecommerce_django/product/models.py
@@ -47,25 +47,21 @@ class Product(models.Model):
 
      def get_thumbnail(self):
           if self.thumbnail:
-               return 'http://127.0.0.1:8000' + self.thumbnail.url
+               return 'http://127.0.0.1:8000/' + self.thumbnail.url
           else:
                if self.image:
                     self.thumbnail = self.make_thumbnail(self.image)
                     self.save()
-                    return 'http://127.0.0.1:8000' + self.thumbnail.url
-               
+                    return 'http://127.0.0.1:8000/' + self.thumbnail.url
                else:
                     return ''
-
      def make_thumbnail(self, image, size=(300, 200)):
-          img = Image.open(image)
+          img= Image.open(image)
           img.convert('RGB')
           img.thumbnail(size)
 
           thumb_io = BytesIO()
-          img.sage(thumb_io, 'JPEG', quality=85)
+          img.save(thumb_io, 'JPEG', quality=85)
 
           thumbnail = File(thumb_io, name=image.name)
-          
           return thumbnail
-
--- ecommerce_django/product/views.py
+++ ecommerce_django/product/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Product
+from .serializers import ProductSerializer
 
-# Create your views here.
+class LatestProductsList(APIView):
+     def get(self, request, format=None):
+          products = Product.objects.all()[0:4]
+          serializers = ProductSerializer(products, many=True)
+          return Response(serializers.data)
